82ba213e6e23e734cbf3871a1f28cddf5794a92d
Tomáš Frixs <tomfrixs@gmail.com>
2025-12-01 23:46:37 +0100

    Init Module Entry System V2

---
+++ ModuleEntrySystemV2/README.md
@@ -0,0 +1,34 @@
+# Module-Entry System V2
+This solution is a bit more complex, so here is a straightforward explanation.
+
+In the new Discord version, reaction role systems are no longer the main method for handling role assignment. Instead, Discord uses "Channels & Roles", where users can assign roles to themselves through a clean built in interface.
+
+The problem is that sometimes you do not want certain roles to be assigned automatically. Some users should only receive them after someone approves the request. Discord does not support this by default. 
+This system provides a way to require approval and then give the role once the request is accepted by using the "Channels & Roles" interface.
+
+---
+
+[YAGPDB](https://yagpdb.xyz/) is a powerful bot, but at the time this was made, it cannot trigger a command when someone is given or removed a role. Its competitor, [Custom Commands](https://ccommandbot.com/) (CC), can do that. The issue is that CC is much more strict and its free plan has very tight limits. It is greedy with resources, which makes anything advanced harder to do.
+
+So how do we work around that?  
+We generate a random message in a hidden channel. This message is used to pass data from CC to YAGPDB. It basically works as a middle man, a temporary cache, a data transfer point, call it whatever you want.
+
+Here is how it works.  
+The CC bot appends data to that message whenever someone is given a specific role. On the YAGPDB side, an interval command (for example every 5 minutes) reads this message. After reading it, YAGPDB adds a reaction to mark that the data was received. CC then before any append sees the reaction, clears the message, and starts adding new data again. Yes, the message can go over the 2000 character limit, but that would only happen on a very active server.
+
+Now we get to the YAGPDB part, where the actual magic happens.
+Based on the queries sent by CC, YAGPDB checks the roles. To access a restricted channel, the user needs only one role, but to keep things smooth and allow the user to remove the role on their own through the "Channels & Roles" interface, we need two roles. A primary one (for example called "/Access/") and a secondary one (for example called "/Access/unverified"). The naming matters for the current implementation. The slashes signal that these roles belong to a module.
+
+On the restricted channel, everyone’s view permission must be blocked. The unverified role must block everything. Only the primary role is allowed to view the channel. When a user selects the option in "Channels & Roles", they receive both roles. This means they can technically see the channel before approval, but only new messages, not the history. And since the interval is five minutes, the system strips the roles again every cycle. This is the only downside, but it is acceptable.
+
+Based on the queries received, an embed with buttons is generated. Only the correct roles can approve or deny it. If denied, the user is informed and both roles are removed. If accepted, only the unverified role is removed, leaving the user with full access.
+
+There is also so-called request role. This role is applied in the "Channels & Roles" interface in each restricted choice along with other 2 roles. This can be used to show waiting channel where all the requests are bieng processed.
+
+---
+
+`process.yag` - Trigger: `Interval`, `5min`
+
+`create_query_for_request.cc` - Custom Command bot, Trigger Type: `On Role Give/Take`, Trigger: `add`
+
+`verify_access.yag` - Trigger Type: Message Component, Trigger: `^(approve|reject)-\d+-\d+$`
---
+++ ModuleEntrySystemV2/create_query_for_request.cc
@@ -0,0 +1,155 @@
+$cooldown[1s]
+// ============ CONFIGURATION ============
+$let[databaseChannelID;1442546776492998762]
+$let[databaseMessageID;1444645737068626051]
+$let[debugChannelID;1442546776492998762]
+$let[notificationChannelID;1444408680924577916]
+$let[databaseHeader;**\[CUSTOMCOMMANDS_AND_YAGPDB_MODULE_ENTRY_DATABASE\]**]
+$let[reactionEmoji;👍]
+$let[getMessageCooldown;60000] // 60 seconds
+$let[timeoutProcessCooldown;70s] // 70 seconds
+
+// DEBUG only:
+// $setServerVar[db_timeout_flag;false]
+// $channelSendMessage[$get[debugChannelID];DEBUG: Triggered command by user: $authorID, with role: $roleID]
+
+// ============ ROLE FORMAT VALIDATION ============
+$if[$regexCheck[$roleName;^/[A-Za-z]+/(unverified)?$]==false]
+  $stop
+$endIf
+
+// ============ SEND USER NOTIFICATION ============
+$channelSendMessage[$get[notificationChannelID];
+<@$authorID>
+{description:Tvoje žádost o přístup byla zaregistrována a bude vytvořena do několika minut.}
+{color:#3694e7}
+{delete:120000}
+;no]
+
+// ============ GET USER UNVERIFIED ROLES ============
+/*
+$let[userRoles;$userRoles[$authorID;names;|]]
+$let[unverifiedRoles;$regexMatch[$get[userRoles];\/[A-Za-z]+\/unverified;g;all;|]]
+$let[data;]
+$if[$get[unverifiedRoles]!=]
+	$let[primaryRoles;$replaceText[$get[unverifiedRoles];unverified;]]
+	$let[data;|$get[primaryRoles]]
+$endIf
+*/
+
+// ============ RECORD PREPARATION ============
+$let[currentTime;$timeStamp]
+$let[newRecord;$authorID/*|$get[currentTime]$get[data]*/]
+$let[timeoutRunning;$getServerVar[db_timeout_flag]] // indicates whether timeout process is running (see the code)
+
+// ============ CHECK LAST GETMESSAGE TIMESTAMP ============
+$let[lastGetTime;$getServerVar[db_last_getMessage]]
+$if[$get[lastGetTime]==||$get[lastGetTime]==undefined]
+	$let[lastGetTime;0]
+$endIf
+$let[timeSince;$sub[$get[currentTime];$get[lastGetTime]]]
+
+// Check the last run of the $getMessage and check if the timeout process is not in the middle of the run.
+$if[$get[timeSince]>$get[getMessageCooldown]&&$get[timeoutRunning]==false]
+	// SAFE TO USE $GETMESSAGE - PROCESS DIRECTLY
+	// $channelSendMessage[$get[debugChannelID];DEBUG: check 1]
+	$setServerVar[db_last_getMessage;$get[currentTime]]
+
+	// Get the content of the database message
+	$let[messageContent;$getMessage[$get[databaseChannelID];$get[databaseMessageID];content]]
+	$if[$checkContains[$get[messageContent];$get[databaseHeader]]==false]
+		$let[messageContent;$get[databaseHeader]]
+	$endIf
+
+	// Check if there is reaction indicating need of refresh
+	$let[messageReaction;$getReactions[$get[databaseChannelID];$get[databaseMessageID];$get[reactionEmoji];id]]
+	$if[$get[messageReaction]==undefined||$get[messageReaction]==none||$get[messageReaction]==]
+		// --- APPEND TO THE EXISTING CONTENT ---
+		$let[headerLength;$textLength[$get[databaseHeader]]]
+		$let[startPosition;$sum[$get[headerLength];0]]
+		$let[currentData;$textSlice[$get[messageContent];$get[startPosition]]]
+
+		$if[$get[currentData]!=&&$get[currentData]!=undefined]
+			$let[newContent;$get[databaseHeader]$get[currentData],$get[newRecord]]
+		$else
+			$let[newContent;$get[databaseHeader]$get[newRecord]]
+		$endIf
+		
+	$else
+		// --- OVERWRITE THE EXISTING CONTENT ---
+		$let[newContent;$get[databaseHeader]$get[newRecord]]
+		$clearReactions[$get[databaseChannelID];$get[databaseMessageID];$get[reactionEmoji]]
+		
+	$endIf
+
+	// Update the database message with the new content
+	$editMessage[$get[databaseMessageID];$get[newContent]]
+
+$else
+	// COOLDOWN ACTIVE - USE BUFFER
+	// $channelSendMessage[$get[debugChannelID];DEBUG: check 2]
+	
+	// Save it to the temporary server variable until we can save it to the database message once cooldown is ready.
+	$let[pendingBuffer;$getServerVar[db_pending_buffer]]
+	$if[$get[pendingBuffer]==||$get[pendingBuffer]==undefined]
+		$setServerVar[db_pending_buffer;$get[newRecord]]
+	$else
+		$setServerVar[db_pending_buffer;$get[pendingBuffer],$get[newRecord]]
+	$endIf
+
+	// Only set timeout process if not already running
+	$let[timeoutRunning;$getServerVar[db_timeout_flag]]
+	$if[$get[timeoutRunning]==false]
+		$setServerVar[db_timeout_flag;true]
+		// $channelSendMessage[$get[debugChannelID];DEBUG: Timeout booting up!]
+		
+		// === === === TIMEOUT PROCESS === === ===
+		// DRY is crying here, but we need to run this because of 1min cooldown on $getMessage
+		$setTimeout[$get[timeoutProcessCooldown]]
+			// $channelSendMessage[$get[debugChannelID];DEBUG: Timeout is starting!]
+			$let[currentTime;$timeStamp]
+			
+			// SAFE TO USE $GETMESSAGE - PROCESS DIRECTLY
+			$setServerVar[db_last_getMessage;$get[currentTime]]
+		
+			// Get the pending records and reset the value
+			$let[pendingRecords;$getServerVar[db_pending_buffer]]
+			$setServerVar[db_pending_buffer;]
+		
+			// Get the content of the database message
+			$let[messageContent;$getMessage[$get[databaseChannelID];$get[databaseMessageID];content]]
+			$if[$checkContains[$get[messageContent];$get[databaseHeader]]==false]
+				$let[messageContent;$get[databaseHeader]]
+			$endIf
+		
+			// Check if there is reaction indicating need of refresh
+			$let[messageReaction;$getReactions[$get[databaseChannelID];$get[databaseMessageID];$get[reactionEmoji];id]]
+			$if[$get[messageReaction]==undefined||$get[messageReaction]==none||$get[messageReaction]==]
+				// --- APPEND TO THE EXISTING CONTENT ---
+				$let[headerLength;$textLength[$get[databaseHeader]]]
+				$let[startPosition;$sum[$get[headerLength];0]]
+				$let[currentData;$textSlice[$get[messageContent];$get[startPosition]]]
+		
+				$if[$get[currentData]!=&&$get[currentData]!=undefined]
+					$let[newContent;$get[databaseHeader]$get[currentData],$get[pendingRecords]]
+				$else
+					$let[newContent;$get[databaseHeader]$get[pendingRecords]]
+				$endIf
+				
+			$else
+				// --- OVERWRITE THE EXISTING CONTENT ---
+				$let[newContent;$get[databaseHeader]$get[pendingRecords]]
+				$clearReactions[$get[databaseChannelID];$get[databaseMessageID];$get[reactionEmoji]]
+				
+			$endIf
+		
+			// Turn off the timeout process FLAG!
+			$setServerVar[db_timeout_flag;false]
+		
+			// Update the database message with the new content
+			$editMessage[$get[databaseMessageID];$get[newContent]]
+			// When the edit overflows 2k characters, it will throw an error.
+		$endTimeout
+		// === === === === === === === === === === === ===
+	$endIf
+$endIf
